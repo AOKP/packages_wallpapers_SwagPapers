cc759ca6f997e93e7b7750f99702f9ccd7f2f86c
STELIX <ssspinni@gmail.com>
2016-01-02 21:27:43 +0100

    SwagPapers - build with org.apache.http.legacy locally
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,6 +3,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_STATIC_JAVA_LIBRARIES += org.apache.http.legacy
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := org.apache.http.legacy:lib/org.apache.http.legacy.jar
+
 LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-under, src)
 
 LOCAL_PACKAGE_NAME := SwagPapers
